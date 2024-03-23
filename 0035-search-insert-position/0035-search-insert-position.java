3ad91bb31a3d945aa041fc5d452a236cf2336a15
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2024-03-23 00:35:05 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (88.06%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -17,22 +17,12 @@ class Solution {
                return binarySearch(arr,0, mid,target);
            }
            
-       }else{
-           return position(arr, target);
-       }
        
+       
+   }else{
+           return low;
+       }
    }
     
-    public int position(int[] arr, int target){
-        int count = -1;
-        for(int i=0;i<arr.length;i++){
-            if(target < arr[i]){
-                return i;
-            }
-        }
-        if(count == -1){
-            return arr.length;
-        }
-        return count;
-    }
+
 }
