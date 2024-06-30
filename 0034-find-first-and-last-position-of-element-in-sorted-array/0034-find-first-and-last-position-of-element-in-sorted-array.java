e0aa5900b7161bcfcb86e46e43eeac0e48eed5e9
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2024-06-29 20:50:44 -0700

    Time: 0 ms (100.00%), Space: 45.8 MB (55.84%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,47 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-     
-        int start = -1, end = -1;
-        for(int i=0; i<nums.length;i++){
-            
-            if(nums[i] == target){
-                if(start == -1){
-                    start = i;
-                }
-                end = i;
-            }
-            
+        int start = 0, end = nums.length - 1;
+        
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
         }
         
-        return new int[]{start, end};
+        int firstPos = findFirst(nums, start, end, target);
+        if (firstPos == -1) {
+            return new int[]{-1, -1}; // target not found
+        }
         
+        int lastPos = findLast(nums, start, end, target);
+        
+        return new int[]{firstPos, lastPos};
     }
+    
+    private int findFirst(int[] nums, int start, int end, int target) {
+        int index = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+    
+    private int findLast(int[] nums, int start, int end, int target) {
+        int index = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target) index = mid;
+        }
+        return index;
+    }
+
 }
