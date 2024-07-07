da40b1101d2d91c9b523addcf8e1285f13624c51
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2024-07-07 16:18:43 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (37.16%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,40 +1,25 @@
 class Solution {
-     public static boolean searchMatrix(int[][] matrix, int target) {
-        int col = matrix[0].length;
-        int row = matrix.length;
-
-        int start = 0, end = row - 1;
-
-        if (row == 1) {
-            return search(matrix, target, 0, col - 1);
+   public boolean searchMatrix(int[][] mat, int target) {
+   if(mat.length == 0){
+            return false;
         }
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            System.out.println("Mid :" + mid);
-
-            if (target >= matrix[mid][0] && target <= matrix[mid][col - 1]) {
-                System.out.println("Inside If");
-                return search(matrix, target, mid, col - 1);
-            } else if (target < matrix[mid][0]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        return false;
-    }
-
-    public static boolean search(int[][] matrix, int target, int row, int endCol) {
-        System.out.println("Inside search " + row + " " + endCol);
-        for (int i = 0; i <= endCol; i++) {
-            System.out.println("Inside search " + matrix[row][i]);
-            if (matrix[row][i] == target) {
+        
+        int n = mat[0].length;
+        int m = mat.length;
+        int i = 0, j = n-1;
+       
+ 
+        while (i < m && j >= 0)
+        {
+            if (mat[i][j] == target)
+            {
                 return true;
             }
+            if (mat[i][j] > target)
+                j--;
+            else 
+                i++;
         }
         return false;
     }
-
 }
