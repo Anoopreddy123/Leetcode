ef164149e8882a182b87aa3501041286ca401b33
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2024-08-28 19:52:49 -0700

    Time: 0 ms (100.00%), Space: 42.2 MB (58.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,26 @@
 class Solution {
     public double myPow(double x, int n) {
- 
-        
-        return (double)Math.pow(x,n);
+        // Handle the case when n is negative
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1.0;
+
+        // Perform exponentiation by squaring
+        while (N > 0) {
+            // If the current bit is 1 (n is odd)
+            if ((N % 2) == 1) {
+                ans = ans * x;
+            }
+            // Square the base for the next bit
+            x = x * x;
+            // Move to the next bit
+            N = N / 2;
+        }
+
+        return ans;
     }
 }
