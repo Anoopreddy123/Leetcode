2c9618021533e2c603f7674b0715898dd7093b14
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2024-11-21 19:55:21 -0800

    Time: 3 ms (91.46%), Space: 44 MB (35.97%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,36 +1,33 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-     
-        
         Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
         
-        int size = nums1.length;
-        int[] arr = new int[size];
+        int size = nums2.length;
+        int[] arr = new int[nums1.length]; // arr should match nums1 length for the final result
         
-        for(int i =0; i < size; i++){
-            arr[i] = -1;
-            int target = nums1[i];
-            boolean found = false;
-            for(int j = 0;j < nums2.length;j++){
-                
-                if(nums2[j] == target){
-                    found = true;
-                }
-                
-                if(nums2[j] > target && found){
-                    arr[i] = nums2[j];
-                    break;
-                
-                }
-                
+        // Traverse nums2 in reverse to find the next greater element
+        for (int i = size - 1; i >= 0; i--) { // Corrected condition (i >= 0 instead of i >= size)
+            
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) { // Ensure stack only has greater elements
+                stack.pop();
             }
             
+            // Assign the next greater element or -1 if stack is empty
+            if (stack.isEmpty()) {
+                hm.put(nums2[i], -1);
+            } else {
+                hm.put(nums2[i], stack.peek());
+            }
             
-            
-            
+            stack.push(nums2[i]); // Push the current element onto the stack
+        }
+        
+        // Populate the result array for nums1 using the hashmap
+        for (int i = 0; i < nums1.length; i++) {
+            arr[i] = hm.get(nums1[i]);
         }
         
         return arr;
-        
     }
 }
