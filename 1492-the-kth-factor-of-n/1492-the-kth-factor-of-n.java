992cdd264641a6387a2b81b5b184571ee20d6626
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2024-12-04 23:07:12 -0800

    Time: 0 ms (100.00%), Space: 41.1 MB (6.58%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -2,11 +2,8 @@ class Solution {
     public int kthFactor(int n, int k) {
         int temp = -1, count = 0;
         for(int i = 1; i <= n && count < k; i++){
-              float div = n % i;
-            System.out.println(div);
-             System.out.println(i);
-             System.out.println(count);
-            if(div == 0.0 ){
+              
+            if(n%i == 0.0 ){
                 
                 count++;
                 temp = i;
