c04525a26996a07b6737d6fff7aed602c8de5a2b
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-08 15:24:33 -0700

    Time: 51 ms (29.69%), Space: 115.3 MB (43.14%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,49 @@
+class LRUCache {
+
+    // capacity of the LRUCache > 0 
+    // return key if exists if not return -1
+    // put 2 conditions 
+        //if update key if it exists 
+        //else add both key-value pair
+    // if capacity is exceeded evict the least used key
+
+    // get and put should be O(1)
+
+   LinkedHashMap<Integer, Integer> cache;
+    int size = 0; 
+
+    public LRUCache(int capacity) {
+        cache = new LinkedHashMap<>();
+        size = capacity;
+    }
+    
+    public int get(int key) {
+        if(cache.containsKey(key)){
+            int temp =  cache.get(key);
+            cache.remove(key);
+            cache.put(key, temp);
+            return temp;
+        }
+
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+    if (cache.containsKey(key)) {
+        cache.remove(key);  // remove to re-insert at the end (most recently used)
+    } else if (cache.size() == size) {
+        // only evict if key is new AND cache is full
+        Map.Entry<Integer, Integer> lru = cache.entrySet().iterator().next();
+        cache.remove(lru.getKey());
+    }
+
+    cache.put(key, value);
+}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
