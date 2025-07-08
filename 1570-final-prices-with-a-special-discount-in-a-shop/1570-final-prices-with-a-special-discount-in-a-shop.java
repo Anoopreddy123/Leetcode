d8cfc503c2a06e0f3a3ea6ce020b3ff7a9f3fe1e
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-07 19:00:39 -0700

    Time: 4 ms (33.97%), Space: 43.3 MB (98.71%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        // we have a array of prices that can be be given a discount j such taht j > i and price[j] <= price[i]1
+        // Using the monotonic stack and trying to change it in place
 
-        // we can give brute force approach if needed using 2 for loops
-
+        Stack<Integer> stack = new Stack<>();
+        //stack.push(prices[0]);
         for(int i = 0; i < prices.length; i++){
-            for(int j = i + 1; j < prices.length; j++){
-                if( prices[j] <= prices[i]){
-                        prices[i] -= prices[j];
-                        break;
-                }
+            while(!stack.isEmpty() && prices[stack.peek()] >= prices[i]){
+                int indx = stack.pop();
+                prices[indx] -= prices[i];
             }
+            stack.push(i);
         }
 
         return prices;
