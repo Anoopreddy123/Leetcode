c23dffdbab8fe98103c14a1aabafde3c8bd6df17
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-08 16:48:14 -0700

    Time: 50 ms (34.59%), Space: 115.2 MB (44.78%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,28 +1,20 @@
 class LRUCache {
-
-    // capacity of the LRUCache > 0 
-    // return key if exists if not return -1
-    // put 2 conditions 
-        //if update key if it exists 
-        //else add both key-value pair
-    // if capacity is exceeded evict the least used key
-
-    // get and put should be O(1)
-
-   LinkedHashMap<Integer, Integer> cache;
-    int size = 0; 
-
+    LinkedHashMap<Integer, Integer> cache;
+    int size = 0;
     public LRUCache(int capacity) {
         cache = new LinkedHashMap<>();
         size = capacity;
     }
     
     public int get(int key) {
+        
         if(cache.containsKey(key)){
-            int temp =  cache.get(key);
-            cache.remove(key);
-            cache.put(key, temp);
-            return temp;
+           int temp = cache.get(key);
+           cache.remove(key);
+
+           cache.put(key, temp);
+
+           return temp;
         }
 
         return -1;
@@ -33,15 +25,15 @@ class LRUCache {
         if(cache.containsKey(key)){
             cache.remove(key);
         }
-        
-        if(cache.size() == size){
-           Map.Entry<Integer,Integer> temp = cache.entrySet().stream().findFirst().get();
 
-            cache.remove(temp.getKey());
+        if(cache.size() == size){
+            Map.Entry<Integer, Integer> temp = cache.entrySet().iterator().next();
+            int lru = temp.getKey();
+
+            cache.remove(lru);
         }
 
-
-        cache.put(key, value);
+        cache.put(key,value);
 
     }
 }
