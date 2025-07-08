e9c4fc5519657b490af57cf78b6947afa2d5db6e
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-08 13:48:45 -0700

    Time: 6 ms (72.2%), Space: 44.8 MB (56.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,33 +1,28 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int max_len = 0;
-        int start_index = 0;
-        if (s.isEmpty()) {
-            return 0;
-        }
-        
-        // String to store the current substring characters
-        String str = "";
-        
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            
-            // Check if the character is already in the substring
-            if (str.contains(String.valueOf(currentChar))) {
-                // Move the start index forward to exclude the previous occurrence of the duplicate character
-                start_index = s.indexOf(currentChar, start_index) + 1;
-                
-                // Rebuild str to reflect the updated window from start_index to i
-                str = s.substring(start_index, i + 1);
-            } else {
-                // Append the character to the current substring
-                str += currentChar;
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        int count = 0, max_count = 0;
+        int start = 0, end = 0;
+
+
+        for(int i = 0; i < s.length(); i++){
+
+            char ch = s.charAt(i);
+
+            if(hashMap.containsKey(ch)){
+                start = Math.max(start, hashMap.get(ch) + 1);
+               
             }
+             hashMap.put(ch, i);
+            end = i;
+            count = (end - start + 1);
             
-            // Update max_len after each character addition
-            max_len = Math.max(max_len, str.length());
+            max_count = Math.max(count, max_count);
+
+
         }
-        
-        return max_len;
+
+        return max_count;
+
     }
 }
