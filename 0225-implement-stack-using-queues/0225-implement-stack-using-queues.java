86f54e7895df59695649563c74e1f5fc3a48bb30
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-08 17:03:17 -0700

    Time: 0 ms (100%), Space: 41.3 MB (81.4%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,31 +1,45 @@
 class MyStack {
-   Queue<Integer> queue = null;
 
-   public MyStack() {
-      queue = new LinkedList<>();
-   }
+    Queue<Integer> main;
+    Queue<Integer> temp_main;
+    int topElement;
+    public MyStack() {
+        main = new LinkedList<>();
+        temp_main = new LinkedList<>();
+    }
     
-   public void push(int x) {
-       queue.add(x);
-       // Rotate the queue to move the newly added element to the front
-       int size = queue.size();
-       for (int i = 1; i < size; i++) {
-           queue.add(queue.poll());
-       }
-   }
+    public void push(int x) {
+        topElement = x;
+        main.add(x);
+
+    }
     
-   public int pop() {
-       return queue.poll();  // This will return the last added element (LIFO behavior)
-   }
+    public int pop() {
+        while(main.size() != 1){
+            int x = main.remove();
+            topElement = x;
+            temp_main.add(x);
+            
+        }
+        int ans = main.remove();
+        main = temp_main;
+        temp_main = new LinkedList<>();
+        return ans;
+    }
     
-   public int top() {
-       return queue.peek();  // This will return the last added element (LIFO behavior)
-   }
+    public int top() {
+        return topElement;
+    }
     
-   public boolean empty() {
-       return queue.isEmpty();
-   }
+    public boolean empty() {
+        if(main.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
+
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack obj = new MyStack();
