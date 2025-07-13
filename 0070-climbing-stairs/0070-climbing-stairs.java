775c7c0f3f0ea1e941607b212793b711ed427cc2
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-13 14:43:21 -0700

    Time: 0 ms (100%), Space: 40.3 MB (65.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,27 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-   
-        if(n == 1 || n == 0) return 1;
-        int i = fib(n);
-        return i;
-   
-    }
-    
-    public static int fib(int n){
-        
-     
-        
-        int arr[] = new int[n + 1];
-        
-         arr[0] = 1; 
-         arr[1] = 1;
-        for(int i = 2; i<=n; i++){
-            
-            arr[i] = arr[i - 1] + arr[i-2];
-            
+        int[] dp = new int[n];
+
+        if(n<=1){
+            return 1;
         }
-        
-        return arr[n];
-        
+
+        dp[0] = 1;
+        dp[1] = 2;
+    
+
+        for(int i = 2; i < n; i++){
+           dp[i] = dp[i-1] + dp[i-2]; 
+        } 
+
+
+        return dp[n - 1];
     }
 }
