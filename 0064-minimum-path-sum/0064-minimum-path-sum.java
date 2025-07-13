9b05a3f599b6f09bded23ce493ee2c4fabe49a6d
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-13 13:17:03 -0700

    Time: 4 ms (61.27%), Space: 47.8 MB (43.75%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,21 +2,23 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int[][] dp = new int[m][n];
-
-        //fill row since only posibility is from the right i.e left side
         dp[0][0] = grid[0][0];
+        // first row since only one entracne from right
+
         for(int i = 1; i < n; i++){
-            dp[0][i] =  dp[0][i-1] + grid[0][i];
+            dp[0][i] = dp[0][i-1] + grid[0][i];
         }
 
+
+        //filling out the first column since there is only one chnace of getting value from top
         for(int i = 1; i < m; i++){
             dp[i][0] = dp[i-1][0] + grid[i][0];
         }
 
-        for(int i = 1; i <m; i++){
-            for(int j = 1; j < n; j++){
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j <n; j++){
                 dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
-                 
             }
         }
 
