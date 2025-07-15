1bc4b83e2cb6d48d0d240822447b9b263108dc74
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-15 14:48:12 -0700

    Time: 138 ms (64.18%), Space: 41.9 MB (36.3%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int row = board.length, cols = board[0].length;
-
-        int[][] visited = new int[row][cols];
-        int index = 0;
-        boolean temp = false;
-        for(int i =0; i < row; i++){
-            temp = false;
-            for(int j = 0; j <cols; j++){
-                //if(visited[i][j] == 0 ) continue;
-                System.out.println(board[i][j] == (word.charAt(index)));
-                if(board[i][j] == (word.charAt(index))){
-                    
-                   temp =  dfs(board, visited, i, j,index, word);
-                    //visited[i][j] = 0;
-                    System.out.println(temp);
-                    if(temp) return true;
+        int m = board.length, n = board[0].length;
+        for(int i = 0; i <m; i++){
+            for(int j = 0; j <n; j++){
+                char ch = word.charAt(0);
+                if(board[i][j] == ch){
+                    if (dfs(i , j, board, word, 0)){
+                        return true;
+                    }
                 }
             }
         }
@@ -23,35 +15,33 @@ class Solution {
         return false;
     }
 
-    public static boolean dfs(char[][] board, int[][] visited, int i, int j, int index, String word){
 
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || index >= word.length() || board[i][j] != (word.charAt(index))){
+    public static boolean dfs(int i, int j, char[][] grid, String word, int index){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j]== '#' || index >= word.length() || grid[i][j] != word.charAt(index)){
             return false;
         }
+
         if(index == word.length() - 1){
             return true;
         }
-        if(board[i][j] != (word.charAt(index)) ){
-            //System.out.print(board[i][j] + "--");
-            return false;
-        }
-
-        char temp = board[i][j];
-         board[i][j] = '#';
-
-
-        index += 1;
         
-         if((dfs(board, visited, i + 1, j, index , word)) || (dfs(board, visited, i, j + 1, index, word)) || (dfs(board, visited, i-1, j, index , word)) ||
-            (dfs(board, visited, i, j-1, index, word))){
-                board[i][j] = temp;
-                return true;
-            }
+        char temp = grid[i][j];
+        if(grid[i][j] == word.charAt(index)){
 
- 
-
-    return false;
-
+          grid[i][j] = '#';
+        }
+        index += 1;
+        if(
+                 dfs(i + 1, j, grid, word, index) ||  dfs(i -  1, j, grid, word, index) ||
+                  dfs(i , j + 1, grid, word, index) ||  dfs(i , j - 1, grid, word, index)
+        ){
+            return true;
+        }else{
+        grid[i][j] = temp;
+        }
+return false;
+       
     }
-    
+
+
 }
