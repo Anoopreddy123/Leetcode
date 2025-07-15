ba62727699aff2581fc5d0999d6b65bae20ab478
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-15 00:55:11 -0700

    Time: 2 ms (96.71%), Space: 41.9 MB (31.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        boolean changed = true;
-
-        while (changed) {
-            String temp = s;
-            s = s.replace("()", "")
-                 .replace("[]", "")
-                 .replace("{}", "");
-            changed = !s.equals(temp);  // check if any replacements occurred
+        Stack<Character> stack = new Stack();
+        for(int i = 0; i< s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(')');
+            }
+            else if(s.charAt(i) == '{'){
+               stack.push('}');
+            }
+              else if(s.charAt(i) == '['){
+              stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop() != s.charAt(i)){
+                 return false;
+            }
+           
         }
-
-        return s.isEmpty();
+        return stack.isEmpty();
     }
 }
