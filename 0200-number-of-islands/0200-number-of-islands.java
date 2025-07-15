96060fedfd49da2e5234aa31aa98ae483593cc6d
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-15 13:24:48 -0700

    Time: 2 ms (99.82%), Space: 49.9 MB (40.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,44 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        // BFS go all possible direction up down left right
-
-        int count = 0;
         int m = grid.length, n = grid[0].length;
+        int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
 
                 if(grid[i][j] == '1'){
-                    bfs(grid, i, j);
-                    count++;
+                     count++;
+                    bfs(i,j,grid);
+                   
                 }
 
             }
         }
 
         return count;
+
     }
 
-    public static void bfs(char[][] grid, int i, int j){
 
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'){
-            return;
+    public void bfs(int i, int j, char[][] grid){
+
+
+        if( i  < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'){
+
+            return ;
         }
 
-
-        if(grid[i][j] == '1'){
-
+        if(grid[i][j] == '1')
             grid[i][j] = '0';
-        }
+        
 
-            bfs(grid, i, j - 1); // up
-            bfs(grid, i, j + 1); // down
-            bfs(grid, i - 1, j); // left
-            bfs(grid, i + 1, j);  // right
+             bfs(i - 1,j,grid); // up
+              bfs(i + 1,j,grid); // down
+               bfs(i,j - 1,grid); // left
+                bfs(i,j + 1,grid); // right
+
+
 
 
     }
+
 }
