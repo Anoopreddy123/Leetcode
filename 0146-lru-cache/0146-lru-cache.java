27dbf09dc7a27bb63627a1dd2af7d96c92931b2a
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-15 02:02:32 -0700

    Time: 49 ms (40.08%), Space: 115.3 MB (41.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,39 +1,72 @@
 class LRUCache {
-    LinkedHashMap<Integer, Integer> cache;
-    int size = 0;
+
+    Node head = new Node(0,0);
+    Node tail = new Node(0,0);
+    int capacity;
+    HashMap<Integer, Node> hashMap;
+
     public LRUCache(int capacity) {
-        cache = new LinkedHashMap<>();
-        size = capacity;
+        this.capacity = capacity;
+         head.next = tail;
+        tail.next = head;
+        hashMap = new HashMap<>();
     }
     
     public int get(int key) {
         
-        if(cache.containsKey(key)){
-           int temp = cache.get(key);
-           cache.remove(key);
-
-           cache.put(key, temp);
-
-           return temp;
+        if(!hashMap.containsKey(key)){
+            return -1;
         }
 
-        return -1;
+        Node node = hashMap.get(key);
+        remove(node);
+        insert(node);
+        return node.val;
     }
     
     public void put(int key, int value) {
-
-        if(cache.containsKey(key)){
-            cache.remove(key);
+        
+        if(hashMap.containsKey(key)){
+                remove(hashMap.get(key));
         }
 
-        if(cache.size() == size){
-            Map.Entry<Integer, Integer> temp = cache.entrySet().iterator().next();
-            int lru = temp.getKey();
-
-            cache.remove(lru);
+        if(hashMap.size() == capacity){
+            remove(tail.prev);
         }
 
-        cache.put(key,value);
+        insert(new Node(key,value));
+
+    }
+
+    public void insert(Node node){
+
+        hashMap.put(node.key, node);
+    Node nextNode = head.next;
+    head.next = node;
+    node.prev = head;
+
+    node.next = nextNode;
+    nextNode.prev = node;
+
+    }
+
+    public void remove(Node node){
+
+        hashMap.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
+    }
+
+    private class Node{
+
+        int key, val;
+        Node prev, next;
+
+        public Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
 
     }
 }
