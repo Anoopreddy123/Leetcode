fdf3c24f292f3abf85360dd972708704fa15d60c
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-15 10:05:43 -0700

    Time: 0 ms (100%), Space: 42.1 MB (68.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-            int start = 0, end = nums.length-1;
+        int size = nums.length;
+        int start = 0, end = size - 1;
 
-            while(start <= end){
+        while(start <= end){
 
-                int mid = start + (end - start) / 2;
-                
-                    if(nums[mid] == target){
-                        return mid;
-                    }
-                    if(nums[start] <= nums[mid]){
+            int mid = start + (end - start)/2;
 
-                        if(target >= nums[start] && target <= nums[mid]){
-                            end = mid - 1;
-                        }
-                         else{
-                        start = mid + 1;
-                    }
-                       
-                    }
-                    else{
-                        if(target >= nums[mid] && target <= nums[end]){
-                          
-                            start = mid + 1;
-                        }
-                         else{
-                          end = mid - 1;
-                    }
-                    }
-                    
+            if (nums[mid] == target) return mid;
             
+            if(nums[start] <= nums[mid]){
+
+                if(target >= nums[start] && target < nums[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+
+            }else{
+                if(target <= nums[end] && target > nums[mid]){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
             }
 
-            return -1;
+        }
 
+        return -1;
     }
 }
