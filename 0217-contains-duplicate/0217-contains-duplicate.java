eb2167baf1c0977a5b775cf93af5a08089e2bbdf
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-18 23:47:32 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        List<Integer> list = new ArrayList<>();
+
+        for(int i  = 0; i < nums.length; i++){
+
+            if(list.contains(nums[i])){
+                return true;
+            }
+
+            list.add(nums[i]);
+
+        }
+
+        return false;
+    }
+}
