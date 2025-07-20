10363d980b862383b9b9d0b9b0e6f3bfcf164dcb
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-19 21:04:58 -0700

    Time: 5 ms (97.02%), Space: 41.8 MB (49.17%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,38 +1,20 @@
 class Solution {
-    public int countSubstrings(String s) {
-        
-       int count = 0;
-        
-        for(int i=0; i< s.length();i++){
-            for(int j=i;j<s.length();j++){
-              boolean flag = isPalendrome(s.substring(i,j + 1)); 
-                if (flag){
-                    count++;
-                }
-            }        
-        }
-        
-        return count;
-        
+    int count =1;
+public int countSubstrings(String s) {
+    if(s.length()==0) 
+        return 0;
+    for(int i=0; i<s.length()-1; i++){
+        checkPalindrome(s,i,i);     //To check the palindrome of odd length palindromic sub-string
+        checkPalindrome(s,i,i+1);   //To check the palindrome of even length palindromic sub-string
     }
-    
-    public boolean isPalendrome(String st){
-        
-    int stLen = st.length(); 
-       int i = 0 , j  = stLen - 1;
+    return count;
+}    
 
-       while(i < j){
-            if(st.charAt(i) == st.charAt(j)){
-                i++;
-                j--;
-                continue;
-            }else{
-                return false;
-            }
-       }
-
-       return true;
-
-        
+private void checkPalindrome(String s, int i, int j) {
+    while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){    //Check for the palindrome string 
+        count++;    //Increment the count if palindromin substring found
+        i--;    //To trace string in left direction
+        j++;    //To trace string in right direction
     }
+}
 }
