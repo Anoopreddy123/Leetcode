f7aa1922f842e28e5bc0de7137aa9f92b0323ac5
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-22 12:05:43 -0700

    Time: 29 ms (16.27%), Space: 51.1 MB (29.77%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -8,32 +8,27 @@ class Solution {
             Arrays.fill(row, -1);
         }
 
-             return    helper(text1, text2, 0, 0, dp);
+        return helper(text1, text2, 0, 0, dp);
 
     }
 
+    public int helper(String text1, String text2, int i, int j, int[][] dp) {
 
-    public int helper(String text1, String text2, int i,int j, int[][] dp){
-
-        if(i == dp.length || j == dp[0].length ){
+        if (i == dp.length || j == dp[0].length) {
             return 0;
         }
 
-        if(dp[i][j] != -1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-
-        if(text1.charAt(i) == text2.charAt(j)){
+        if (text1.charAt(i) == text2.charAt(j)) {
             return 1 + helper(text1, text2, i + 1, j + 1, dp);
-        }else{
-            dp[i][j] = Math.max(helper(text1, text2, i+1,j, dp), helper(text1, text2, i,j+1, dp));
+        } else {
+            dp[i][j] = Math.max(helper(text1, text2, i + 1, j, dp), helper(text1, text2, i, j + 1, dp));
         }
 
-
         return dp[i][j];
-
-
 
     }
 }
