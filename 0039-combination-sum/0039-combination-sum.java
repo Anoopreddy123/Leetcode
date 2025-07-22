4b9bcc658b0e794a01f8a8bbbe062a5288ae1736
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-22 02:39:42 -0700

    Time: 2 ms (82.85%), Space: 44.8 MB (45.29%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-            List<List<Integer>> res = new ArrayList<>();
-             helper(candidates, target, res, 0, 0, new ArrayList<Integer>());
+        List<List<Integer>> res = new ArrayList<>();
 
-            return res;
+        helper(candidates, target, res, 0, 0, new ArrayList<>());
+        return res;
     }
 
-    public void helper(int[] candidates, int target, List<List<Integer>> list, int sum, int index, ArrayList<Integer> ans){
+    public void helper(int[] candidates, int target, List<List<Integer>> res, int sum, int index, List<Integer> ans){
 
         if(sum == target){
-          list.add(new ArrayList<>(ans));
+            res.add(new ArrayList<>(ans));
           return;
         }
 
-        if(sum > target || index >= candidates.length){
-            return;
-        }
+        if(index >= candidates.length || sum > target) return;
 
         ans.add(candidates[index]);
-       helper(candidates, target, list, sum + candidates[index], index,ans);
-       ans.remove(ans.size()-1);
-        helper(candidates, target, list, sum, index + 1, ans);
+
+        helper(candidates, target, res, sum + candidates[index], index, ans);
+
+        ans.remove(ans.size() - 1);
+
+     helper(candidates, target, res, sum , index + 1, ans);
+
+
+
 
     }
 }
