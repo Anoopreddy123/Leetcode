320cf4a7f9e601d38c2a8ab4989c3920b6108e9f
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-21 22:23:49 -0700

    Time: 2 ms (96.88%), Space: 42 MB (80.09%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.java
@@ -1,57 +1,54 @@
 class Solution {
+
     int[] parent = new int[26];
+
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
         
-        int size = s1.length();
+        for(int i = 0; i < 26; i++){
 
-       for(int i =0; i < 26; i++){
             parent[i] = i;
-       }
 
+        }
 
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < s1.length(); i++){
             union(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
         }
 
-
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < baseStr.length(); i++){
-
+            
             char c = baseStr.charAt(i);
-            
-          sb.append((char)(find(c - 'a') + 'a'));
-            
-        }
-
-
-                    return sb.toString();
-
-    }
-
-     private  int find(int x){
-
-        if(parent[x] != x){
-            
-            parent[x] = find(parent[x]);
+            sb.append((char) (find(c - 'a')+ 'a'));
 
         }
-
-        return parent[x];
-
+        return sb.toString();
     }
 
-
-    public  void union(int x, int y){
+    public void union(int x, int y){
 
         int rootX = find(x);
         int rootY = find(y);
-        if (rootX == rootY) return;
+
+        if(rootX == rootY){
+            return;
+        }
+
         if(rootX < rootY){
             parent[rootY] = rootX;
         }else{
             parent[rootX] = rootY;
         }
 
+    }
+
+    public int find(int x){
+
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+
+        return parent[x];
 
     }
+
 }
