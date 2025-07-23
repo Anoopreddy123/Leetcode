1803af37ba3e9ce94d083a4a091f48cfd139f9c5
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-23 12:48:23 -0700

    Time: 2 ms (63.04%), Space: 44.2 MB (70.68%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.java
+++ 1184-car-pooling/1184-car-pooling.java
@@ -1,37 +1,21 @@
-import java.util.*;
-
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int n = trips.length;
-        int[] start = new int[n];
-        int[] end = new int[n];
+        int[] count = new int[1001];
 
-        for (int i = 0; i < n; i++) {
-            start[i] = trips[i][1];
-            end[i] = trips[i][2];
+
+        for(int i = 0; i < trips.length; i++){
+
+            count[trips[i][1]] += trips[i][0];
+            count[trips[i][2]] -= trips[i][0];
+
         }
 
-        Arrays.sort(start);
-        Arrays.sort(end);
-
-        // But this is incomplete because we also need passengers per trip
-        // So we need an event-based approach
-        List<int[]> events = new ArrayList<>();
-        for (int[] trip : trips) {
-            events.add(new int[]{trip[1], trip[0]}); // pickup
-            events.add(new int[]{trip[2], -trip[0]}); // dropoff
-        }
-
-        // Sort by location
-        Collections.sort(events, (a, b) -> {
-    if (a[0] != b[0]) return a[0] - b[0];
-    return a[1] - b[1]; // Negative before positive
-});
-
-        int currentPassengers = 0;
-        for (int[] event : events) {
-            currentPassengers += event[1];
-            if (currentPassengers > capacity) return false;
+        int sum = 0;
+        for(int i : count){
+            sum += i;
+            if( sum > capacity){
+                return false;
+            }
         }
         return true;
     }
