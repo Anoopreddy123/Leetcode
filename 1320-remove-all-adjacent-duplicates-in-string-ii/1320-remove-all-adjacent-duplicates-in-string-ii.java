beb974a0d7ec323ba924d40ce22c29f2a05c85ac
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-24 07:47:31 -0700

    Time: 55 ms (14.79%), Space: 46.4 MB (20.05%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,30 +1,45 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<Character> stack = new Stack<>();
         Stack<Integer> count = new Stack<>();
+        Stack<Character> chars = new Stack<>();
+        int strLen = s.length();
+        int left = 0;
+        int right = 0;
+        int counts = 0;
+        while(right < strLen){
+            char c = s.charAt(right);
+            if(!count.isEmpty() && chars.peek() == c){
 
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && stack.peek() == c) {
-                 count.push(count.pop() + 1);
-            } else {
-              count.push(1);
+                count.push(count.pop() + 1);
+
+            }else{
+                count.push(1);
             }
 
-            stack.push(c);
+            chars.push(c);
 
-            if (count.peek() == k) {
-                int cnt = k;
-                while (cnt-- > 0 && !stack.isEmpty()) {
-                    stack.pop();
+            if(count.peek() ==  k){
+                counts = count.pop();
+
+                while(counts-- > 0){
+                    chars.pop();
                 }
-               count.pop(); 
+
+                //count.pop();
+
             }
+
+            right++;
+
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
+
+        while(!chars.isEmpty()){
+            sb.append(chars.pop());
         }
+
+
         return sb.reverse().toString();
     }
 }
