17c51a0d4f8149b28bd4d274398a7571547bf762
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-07-31 00:01:24 -0700

    Time: 2 ms (21.71%), Space: 40.9 MB (74.46%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,55 +1,46 @@
 class Solution {
-    public String fractionToDecimal(int num, int den) {
-
-      
+    public String fractionToDecimal(int numerator, int denominator) {
+        if(numerator == 0 || denominator == 0){
+            return "0";
+        }
 
         StringBuilder sb = new StringBuilder();
-
-        if ((num < 0) ^ (den < 0)) {
+  if( (numerator < 0) ^  (denominator < 0)){
             sb.append("-");
         }
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
 
-        long numerator = Math.abs((long) num);
-long denominator = Math.abs((long) den);
-  if(numerator == 0 || denominator == 0){
-            return "0";
-            
-        }
-          
+        long rem = num % den;
+        long quo = num / den;
+
       
- long rem = Math.abs((long)numerator % denominator);
-        long quo = Math.abs((long)numerator/ denominator);
-
-        sb.append(String.valueOf(quo));
+        
+        sb.append(quo);
+        
         if(rem == 0){
-            
             return sb.toString();
-        }else{
-            sb.append(".");
-            HashMap<Long, Integer> hashMap = new HashMap<>();
+        }
 
-            while(rem != 0){
+        sb.append(".");
+        HashMap<Long,Integer> hashMap = new HashMap<>();
+        while(rem != 0){
 
-                    if(hashMap.containsKey(rem)){
-                        sb.insert(hashMap.get(rem), "(");
-                        sb.append(")");
+            if(hashMap.containsKey(rem)){
 
-                        return sb.toString();
-                    }
-
-                    hashMap.put(rem, sb.length());
-
-                    rem = rem * 10;
-                    sb.append(rem/denominator);
-
-                    rem %= denominator;
+                sb.insert( hashMap.get(rem) , "(" );
+                sb.append(")");
+                return sb.toString();
 
             }
 
+            hashMap.put(rem, sb.length());
+
+            rem = rem * 10;
+            sb.append(rem / den);
+            rem = rem % den;
         }
 
-
-        return sb.toString();
-
+            return sb.toString();
     }
 }
