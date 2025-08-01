04e05e5da20911ba05724235ba86cb376ec7e88e
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-01 01:36:57 -0700

    Time: 1 ms (99.49%), Space: 44.5 MB (15.04%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,21 +1,19 @@
 class Solution {
     public int compress(char[] chars) {
-         int right = 0;
-      
+         int left = 0, right = 0;
         int index = 0;
-        while(right < chars.length){
-              char temp = chars[right];
-       int count = 0;
-            while(right < chars.length && chars[right] == temp){
-                count++;
-                 right++;
-            }
+         while(right < chars.length){
+            char temp = chars[right];
+             int count = 0;
+                while(right < chars.length && chars[right] == temp){
+                    count++;
+                    right++;
+                }
             chars[index++] = temp;
-            //temp = chars[right];
-  
-           if (count > 1) {
+
+            if(count > 1){
                 String str = String.valueOf(count);
-                for (char c : str.toCharArray()) {
+                for(char c : str.toCharArray()){
                     chars[index++] = c;
                 }
             }
@@ -23,5 +21,6 @@ class Solution {
         }
 
         return index;
+
     }
 }
