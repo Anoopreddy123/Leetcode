885dd12fc55fe1a36a6d2aba99862030b5f379bf
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-05 01:49:41 -0700

    Time: 17 ms (59.2%), Space: 45 MB (45.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,37 @@
 class Solution {
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
-
-        String result = "";
         int max_len = 0;
+        String longer = "";
+        int len = s.length();
+        for(int i = 0; i < len; i++){
 
-        for (int i = 0; i < s.length(); i++) {
-            String odd = expand(s, i, i);      // odd-length
-            String even = expand(s, i, i + 1); // even-length
+            String even = palendrome(i, i, s);
+            String odd = palendrome(i, i+1, s);
 
-            String longer = odd.length() > even.length() ? odd : even;
+            String temp = even.length() > odd.length() ? even : odd;
 
-            if (longer.length() > max_len) {
-                max_len = longer.length();
-                result = longer;
+            if(temp.length() > max_len){
+                max_len = temp.length();
+                longer = temp;  
             }
+
+            
         }
-        return result;
+
+        return longer;
+
     }
 
-    private String expand(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    public String palendrome(int start, int end, String s){
+
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
         }
-        // After loop breaks, left and right have gone one step too far
-        return s.substring(left + 1, right);
+
+
+        return s.substring(start + 1, end);
+
     }
 }
