2c21212e0cdcfa194616ae36f58ec7749dcc2836
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-09 13:04:11 -0700

    Time: 4 ms (95.67%), Space: 44.5 MB (82.05%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        //int horizontal = 0, vertical = 0;
-        int[] position = new int[]{0,0};
-        for(char ch : moves.toCharArray()){
+      int ver = 0, hor = 0;
 
-            if(ch == 'U'){
-                position[0] -= 1;
-            }else if(ch == 'D'){
-                position[0] += 1;
-            }else if(ch == 'L'){
-                position[1] -= 1;
-            }else{
-                position[1] +=1;
-            }
+      for(char c : moves.toCharArray()){
 
+        if( c == 'U' ) {
+            ver++;
+        } else if (c == 'D')
+        {
+            ver--;
+        }else if(c == 'L'){
+            hor++;
+        }else{
+            hor--;
         }
 
+      }
 
-        return position[0] == 0 && position[1] == 0;
+
+      return ver == 0 && hor == 0;   
     }
 }
