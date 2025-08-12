57eee421754460877abfe3bde35d910f6de4416c
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-11 23:12:42 -0700

    Time: 1 ms (100%), Space: 44.2 MB (71%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.java
+++ 1184-car-pooling/1184-car-pooling.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int[] count = new int[1001];
-
-
         for(int i = 0; i < trips.length; i++){
 
             count[trips[i][1]] += trips[i][0];
