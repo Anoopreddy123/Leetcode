3c4c68553d4922274ed0c9e1050b30af8e217948
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-16 02:23:21 -0700

    Time: 3 ms (13.5%), Space: 40.6 MB (56.69%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,10 +1,6 @@
 class Solution {
     public int maximum69Number (int num) {
-        /*
-         * Replaces the first occurrence of "6" with "9" to maximize the number.
-         * * Time Complexity: O(log10(num)) - for number to string conversion.
-         * Space Complexity: O(log10(num)) - to store the string representation.
-         */
+
         String numStr = Integer.toString(num);
         return Integer.parseInt(numStr.replaceFirst("6", "9"));
     }
