7ef5ea4c8c7142949742072a698567cfb0fd0baa
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-18 11:53:44 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-    int n = spells.length, m = potions.length;
-    int[] ans = new int[n];
-    int[][] spellsWithIdx = new int[n][2];
-    for (int i = 0; i < n; i++) {
-        spellsWithIdx[i][0] = spells[i];
-        spellsWithIdx[i][1] = i;
-    }
+        int n = spells.length, m = potions.length;
+        int[] ans = new int[n];
+        Arrays.sort(potions);
 
-    Arrays.sort(spellsWithIdx, (a, b) -> Integer.compare(a[0], b[0]));
-    Arrays.sort(potions);
-
-    int j = m - 1;
-    for (int i = 0; i < n; i++) {
-        int spell = spellsWithIdx[i][0];
-        while (j >= 0 && (long) spell * potions[j] >= success) {
-            j--;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((long) spells[i] * potions[j] >= success) {
+                    ans[i] = m - j;
+                    break;  // stop at the first valid potion
+                }
+            }
         }
-        ans[spellsWithIdx[i][1]] = m - (j + 1);
+        return ans;
     }
-
-    return ans;
-}
 }
