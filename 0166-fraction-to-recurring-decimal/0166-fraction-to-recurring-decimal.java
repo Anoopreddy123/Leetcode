bed9aff7adfb2a7ec5e114fc424b1a486de49ddb
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-25 23:39:04 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,48 +1,50 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
+        StringBuilder sb = new StringBuilder();
         
-        if(numerator == 0){
+
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+
+        if(num == 0 || den == 0){
             return "0";
         }
 
-       
-        StringBuilder sb = new StringBuilder();
-          if(numerator < 0 ^ denominator < 0){
+        if(numerator < 0 || denominator < 0){
             sb.append("-");
         }
- long num = Math.abs((long) numerator);
-        long den = Math.abs((long) denominator);
-
+        
         long rem = num % den;
         long quo = num / den;
+
         sb.append(quo);
 
         if(rem == 0){
             return sb.toString();
         }
 
-        sb.append('.');
-
+        sb.append(".");
         HashMap<Long, Integer> hashMap = new HashMap<>();
-
         while(rem != 0){
 
             if(hashMap.containsKey(rem)){
                 sb.insert(hashMap.get(rem), "(");
-                sb.append(')');
+                sb.append(")");
+
                 return sb.toString();
             }
+
             hashMap.put(rem, sb.length());
+
             rem = rem * 10;
             sb.append(rem/den);
             rem = rem % den;
 
 
+
+
         }
 
         return sb.toString();
-
-
-
     }
 }
