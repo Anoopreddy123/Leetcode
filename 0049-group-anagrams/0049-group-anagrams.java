43646cf6d00f8998739f8e74bea5b15c9d0d2407
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-26 00:41:52 -0700

    Time: 6 ms (98.66%), Space: 48.1 MB (34.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<String,ArrayList<String>> hashMap = new HashMap<>();
 
         for(String s : strs){
-            ArrayList<String> temp = new ArrayList<>();
+           
             char[] c = s.toCharArray();
 
             Arrays.sort(c);
@@ -13,6 +13,7 @@ class Solution {
             if(hashMap.containsKey(sortedString)){
                hashMap.get(sortedString).add(s);
             }else{
+                 ArrayList<String> temp = new ArrayList<>();
                 temp.add(s);
                 hashMap.put(sortedString, temp);
             }
