84b6125d63a4ed934b7bdd114e62e731228597f8
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-08-25 23:25:18 -0700

    Time: 6 ms (80.45%), Space: 45.4 MB (47.75%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,23 +3,18 @@ class Solution {
         int[] freq = new int[26];
 
         for(char c : s.toCharArray()){
-
             freq[c - 'a']++;
-
         }
 
+
         for(int i = 0; i < s.length(); i++){
-
             char c = s.charAt(i);
-
             if(freq[c - 'a'] == 1){
                 return i;
             }
-
         }
 
+
         return -1;
-
-
     }
 }
