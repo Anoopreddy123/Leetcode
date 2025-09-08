0fc811988bf0649a2001b335640fc23cd35ea5a7
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-09-08 03:38:55 -0700

    Time: 7 ms (98.68%), Space: 46.6 MB (73.9%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,8 +1,37 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        // Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
+        // int[] currentInterval = intervals[0];
+
+        // ArrayList<int[]> arrayList = new ArrayList<>();
+
+        // for(int[] interval : intervals){
+
+        //     int currentStart = currentInterval[0];
+        //     int currentEnd = currentInterval[1];
+
+        //     int nextStart = interval[0];
+        //     int nextEnd = interval[1];
+
+        //     if(currentEnd >= nextStart){
+
+        //         currentInterval[1] = Math.max(currentEnd, nextEnd);
+
+        //     }else{
+
+        //         arrayList.add(currentInterval);
+        //         currentInterval = interval;
+
+        //     }
+
+        // }
+        //     arrayList.add(currentInterval);
+
+        //     return arrayList.toArray(new int[arrayList.size()][]);
+
+
         Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
         int[] currentInterval = intervals[0];
-
         ArrayList<int[]> arrayList = new ArrayList<>();
 
         for(int[] interval : intervals){
@@ -14,20 +43,19 @@ class Solution {
             int nextEnd = interval[1];
 
             if(currentEnd >= nextStart){
-
                 currentInterval[1] = Math.max(currentEnd, nextEnd);
-
             }else{
-
-                arrayList.add(currentInterval);
+                 arrayList.add(currentInterval);
                 currentInterval = interval;
-
+                // arrayList.add()
             }
 
         }
-            arrayList.add(currentInterval);
 
-            return arrayList.toArray(new int[arrayList.size()][]);
+        arrayList.add(currentInterval);
+
+
+        return arrayList.toArray(new int[arrayList.size()][]);
 
     }
 }
