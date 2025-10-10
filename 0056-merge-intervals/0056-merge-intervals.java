fa67d3ba14eb5f1155325a4d11e755acf2af84f0
Anoopreddy123 <62228663+Anoopreddy123@users.noreply.github.com>
2025-10-10 02:05:23 -0700

    Time: 7 ms (98.4%), Space: 46.6 MB (73.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,43 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
-        // int[] currentInterval = intervals[0];
-
-        // ArrayList<int[]> arrayList = new ArrayList<>();
-
-        // for(int[] interval : intervals){
-
-        //     int currentStart = currentInterval[0];
-        //     int currentEnd = currentInterval[1];
-
-        //     int nextStart = interval[0];
-        //     int nextEnd = interval[1];
-
-        //     if(currentEnd >= nextStart){
-
-        //         currentInterval[1] = Math.max(currentEnd, nextEnd);
-
-        //     }else{
-
-        //         arrayList.add(currentInterval);
-        //         currentInterval = interval;
-
-        //     }
-
-        // }
-        //     arrayList.add(currentInterval);
-
-        //     return arrayList.toArray(new int[arrayList.size()][]);
-
-
         Arrays.sort(intervals, (arr1, arr2) -> arr1[0] - arr2[0]);
+        ArrayList<int[]> list = new ArrayList<>();
+
         int[] currentInterval = intervals[0];
-        ArrayList<int[]> arrayList = new ArrayList<>();
 
         for(int[] interval : intervals){
 
             int currentStart = currentInterval[0];
-            int currentEnd = currentInterval[1];
+            int currentEnd =  currentInterval[1];
 
             int nextStart = interval[0];
             int nextEnd = interval[1];
@@ -45,17 +16,13 @@ class Solution {
             if(currentEnd >= nextStart){
                 currentInterval[1] = Math.max(currentEnd, nextEnd);
             }else{
-                 arrayList.add(currentInterval);
+                list.add(currentInterval);
                 currentInterval = interval;
-                // arrayList.add()
             }
 
         }
+        list.add(currentInterval);
 
-        arrayList.add(currentInterval);
-
-
-        return arrayList.toArray(new int[arrayList.size()][]);
-
+return list.toArray(new int[list.size()][]);
     }
 }
